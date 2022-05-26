8bfddbbc436de174ff458b1caecb9f25f7730a55
Shreenidhi Bharadwaj H K <shreenidhihk@outlook.com>
2022-05-27 02:15:19 +0530

    Initial Commit

---
+++ vars/BuildMaven.groovy
@@ -0,0 +1,17 @@
+class BuildMaven {
+    private final Script script
+
+    BuildMaven(Script script) {
+        this.script = script
+    }
+
+    // You can pass as many parameters as needed
+    void execute(String name, boolean param1) {
+        script.echo "Triggering script with name == ${name}"
+        script.sh "echo 'Execute your desired bash command here'"
+
+        if (param1) {
+            script.sh "echo 'Executing conditional command, because param1 == true'"
+        }
+    }
+}
