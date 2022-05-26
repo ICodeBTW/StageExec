22d3563e475e52de1717f557038cdc7d9553bd1a
Shreenidhi Bharadwaj H K <shreenidhihk@outlook.com>
2022-05-27 02:59:33 +0530

    Added Scripted

---
+++ vars/BuildMavenSP.groovy
@@ -0,0 +1,19 @@
+class BuildMavenSP{
+    private final Script script
+
+    BuildMavenSP(Script script){
+        this.script  = script
+    }
+
+    void execute(String name, boolean param1) {
+        script.stage(name) {
+            script.echo "Triggering ${name} stage..."
+            script.sh "echo 'Execute your desired bash command here'"
+            script.echo this.script.env.VERSION
+
+            if (param1) {
+                script.sh "echo 'Executing conditional command, because param1 == true'"
+            }
+        }
+    }
+}
